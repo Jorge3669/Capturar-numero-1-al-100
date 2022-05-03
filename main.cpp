fe6e709cab0076b7b51691aed2849873e0492121
Jorge3669 <101146927+Jorge3669@users.noreply.github.com>
2022-05-02 21:36:46 -0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+/* Capturar un número cualesquiera e informar si es o no es mayor de 100 */
+using namespace std;
+int main(int argc, char** argv) {
+	
+	
+	
+	int numero;
+	
+	cout<<"Ingrese cualquier numero"<<endl;
+	cin>>numero;
+if (numero>100){
+	cout<<"EL NUMERO INGRESADO ES MAYOR A 100"<<endl;
+}
+else{
+	cout<<"EL NUMERO INGRESADO ES MENOR A 100"<<endl;
+}
+	system("pause");
+}
